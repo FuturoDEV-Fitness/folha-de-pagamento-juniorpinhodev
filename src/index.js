352c7fee989e73634bb2c0a6b8fffb5cc5e39264
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-23 14:27:37 -0300

    feat: 🎸 Criação do Programa principal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-const calculo_inss = require('./calculo_inss');
+const readline = require('readline')
+const calcINSS = require('./calculo_inss');
+const calcIR = require('./calculo_imposto_renda')
+const calcLiquidPay = require('./calculo_salario_liquido')
 
-console.log(calculo_inss(3000))
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let name = ""
+input.question("Por favor, digite seu nome completo:", (nameImput) => {
+    name = nameImput
+
+    let CPF = 0
+    input.question("Por favor, digite seu CPF:", (cpfImput) => {
+        CPF = cpfImput
+
+        let month = 0
+        input.question("Por favor, digite o mês do pagamento:", (monthImput) => {
+            month = monthImput
+
+            let grossPay = 0
+            input.question("Por favor, digite o seu salário bruto:", (grossPayImput) => {
+                grossPay = grossPayImput
+
+                const INSS = calcINSS(grossPay)
+                const IR = calcIR(grossPay)
+                const liquidPay = calcLiquidPay(grossPay)
+
+                console.log('\n___ Folha de Pagamento ___')
+                console.log(`Nome: ${name}`)
+                console.log(`CPF: ${CPF}`)
+                console.log(` ======================== `)
+                console.log(`Mês do Pagamento: ${month}`)
+                console.log(`Salário Bruto: R$${grossPay}`)
+                console.log(`INSS: R$${INSS}`)
+                console.log(`Imposto de Renda: R$${IR}`)
+                console.log(`Outros Descontos: R$0,00`)
+                console.log(` ======================== `)
+                console.log(`Salário Líquido: R$${liquidPay}`)
+
+                input.close()
+            })
+        })
+    })
+})
